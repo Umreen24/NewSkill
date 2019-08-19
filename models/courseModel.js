29c666a9b076018111d461a1e4d65b7828f3cc1b
Antonio Tobar <antoniotobar@Antonios-MacBook-Air.local>
2019-08-19 14:41:34 -0500

    "new schema"

--- models/courseModel.js
+++ models/courseModel.js
@@ -62,7 +62,7 @@ const courseSchema = new mongoose.Schema(
     },
     imageCover: {
       type: String,
-      require: [true, 'A tour must have a cover image']
+      require: [true, 'A course must have a cover image']
     },
     images: [String],
     createdAt: {
@@ -71,7 +71,7 @@ const courseSchema = new mongoose.Schema(
       select: false
     },
     startDates: [Date],
-    secretTour: {
+    secretCourse: {
       type: Boolean,
       default: false,
       select: false
