c74dd12cb0d15e2b4dfca124a1640348e4a719bb
Taliaa <taliaa10@ymail.com>
2019-08-23 17:30:45 -0500

    trying to fix login

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,29 @@
-
 // /* eslint-disable */
-const login = async (email,password) =>{
-  try{
-   const res = await axios({
-     method: 'POST',
-     url: 'http://127.0.0.1:8000/api/v1/users/login',
-     data: {
-       email,
-       password 
-     }
-  });
- 
-   if(res.data.status === 'success') {
-     alert('Logged in successfully');
-     window.setTimeout(() => {
-       location.assign('/overview');
-     }, 1500);
-   }
+const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+
+    if (res.data.status === 'success') {
+      alert('Logged in successfully');
+      window.setTimeout(() => {
+        location.assign('/overview');
+      }, 1500);
+    }
   } catch (err) {
     alert(err);
   }
- };
- 
- 
- 
- document.querySelector('.form').addEventListener('submit', e => {
-   e.preventDefault();
-   const email = document.getElementById('email').value;
-   const password = document.getElementById('password').value 
-   login(email,password);
- });
+};
+
+document.querySelector('.form').addEventListener('submit', e => {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  login(email, password);
+});
