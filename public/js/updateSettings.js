fcef46dd57f2ecd1cc4ccca80754ad1e19a5dfd0
Umreen <uidigitalcrafts@gmail.com>
2019-08-24 01:42:23 -0500

    trying to fix update password functions

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,30 +1,54 @@
 /* eslint-disable */
 
-const updateMyData = async (name, email) => {
+const updateMySettings = async (data, type) => {
   try {
+    const url = 
+    type === 'password' ? 
+    'http://127.0.0.1:8000/api/v1/users/updateMyPassword' :
+    'http://127.0.0.1:8000/api/v1/users/updateMe'
+  
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:8000/api/v1/users/updateMe',
-      data: {
-        name,
-        email
-      }
+      url,
+      data
     });
+
     if (res.data.status === 'success') {
-      showAlert('success', 'Changes made successfully!')
+      showAlert('success', `${type.toUpperCase()} updated successfully!`)
     }
+
   } catch (err) {
     showAlert('error', err.response.data.message); 
-  };
+  }
 };
 
-const updateData = document.querySelector('.form-user-data')
+const userDataForm = document.querySelector('.form-user-data')
 
-if(updateData) {
-  updateData.addEventListener('submit', e => {
+if(userDataForm) {
+  userDataForm.addEventListener('submit', e => {
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value; 
-    updateMyData(name,email);
+    updateMySettings({name,email}, 'data');
+  });
+}
+
+const userPasswordForm = document.querySelector('.form-user-password')
+
+if(userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...'
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateMySettings({passwordCurrent, password, passwordConfirm }, 'password');
+
+    document.querySelector('.btn--save-password').textContent = 'Save password'
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+
   });
 }
