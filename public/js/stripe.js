d076cdb3a4fb7294e9e4d6e0f7d662110191980f
Taliaa <taliaa10@ymail.com>
2019-08-24 13:30:03 -0500

    home and overview styling

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,26 +1,21 @@
 import axios from 'axios';
-import{showAlert} from './alerts';
-const stripe = Stripe('pk_test_Z41e7qzU8XB6wTKWfoNyqCmG0089uJGA1H');
+import { showAlert } from './alerts';
+const stripe = Stripe('sk_test_mUwY18tilOtl6UL89EcJG8EY00AqzZhm4p');
 
-export const bookCourse = async courseId =>{
+export const bookCourse = async courseId => {
+  try {
+    //checkout from API
 
-try{
-//checkout from API
-
-const session = await axios(
-    `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${courseId}`);
+    const session = await axios(`api/v1/bookings/checkout-session/${courseId}`);
 
     console.log(session);
 
     await stripe.redirectToCheckout({
-        sessionId: session.data.session.id
+      sessionId: session.data.session.id
     });
-}catch(err){
-console.log(err)
+  } catch (err) {
+    console.log(err);
 
-showAlert('error', err);
+    showAlert('error', err);
+  }
 };
-
-
-};
-    
