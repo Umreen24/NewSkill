e9a447a3ff26da88cd620f63a2debb44c6b1da2f
Taliaa <taliaa10@ymail.com>
2019-08-25 03:06:15 -0500

    booking alerts

---
+++ public/js/alerts.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+//type is either 'success' or 'error'
+const showAlert = (type, msg) => {
+  hideAlert();
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 5000);
+};
