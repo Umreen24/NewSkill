10446c2fde2f18404fb5f6ee65c98809a1e5ddf2
Umreen <uidigitalcrafts@gmail.com>
2019-08-25 16:31:16 -0500

    issues with password reset

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+// const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
@@ -147,76 +147,83 @@ exports.restrictTo = (...roles) => {
   };
 };
 
-exports.forgotPassword = catchAsync(async(req, res, next) => {
-  // 1) Get user based on POSTed email 
-  const user = await User.findOne({email: req.body.email});
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
   if (!user) {
-    return next(new AppError('There is no user with that email address.', 404));
+    return next(new AppError('There is no user with email address.', 404));
   }
-  // 2) Generate the random reset token 
+
+  // 2) Generate the random reset token
   const resetToken = user.createPasswordResetToken();
-  await user.save({validateBeforeSave: false});
-  // 3) Send it to the user's email
-  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+  await user.save({ validateBeforeSave: false });
 
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you did not forget your password, please ignore this email!`;
+  // 3) Send it to user's email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
 
-  try{
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+
+  try {
     await sendEmail({
       email: user.email,
-      subject: 'Your Passowrd Reset Token (valid for 10 min)',
+      subject: 'Your password reset token (valid for 10 min)',
       message
-    })
-  
+    });
+
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email!'
     });
-  } catch(err) {
+  } catch (err) {
     user.passwordResetToken = undefined;
-    user.passwordResetExipres = undefined;
-    await user.save({validateBeforeSave: false});
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
 
-    return next(new AppError('There was an error sending the email. Try again later!', 500));
-  };
+    return next(
+      new AppError('There was an error sending the email. Try again later!'),
+      500
+    );
+  }
 });
 
-exports.resetPassword = catchAsync(async(req, res, next) => {
-  // 1) Get user based on the token 
-  const hashedToken = crypto
-    .createHash('sha256')
-    .update(req.params.token)
-    .digest('hex');
+// exports.resetPassword = catchAsync(async(req, res, next) => {
+//   // 1) Get user based on the token 
+//   const hashedToken = crypto
+//     .createHash('sha256')
+//     .update(req.params.token)
+//     .digest('hex');
   
-    const user = await User.findOne({
-    passwordResetToken: hashedToken, 
-    passwordResetExipres: {$gt:Date.now()} });
+//     const user = await User.findOne({
+//     passwordResetToken: hashedToken, 
+//     passwordResetExipres: {$gt:Date.now()} });
 
-  // 2) If token has not expired, and there is a user, set the new password
-  if (!user) { 
-    return next(new AppError('Token is invalid or has expired', 400))
-  }
-  user.password = req.body.password;
-  user.passwordConfirm = req.body.passwordConfirm;
-  user.passwordResetToken = undefined;
-  user.passwordResetExpires = undefined;
-  await user.save();
-  // 3) Update changedPasswordAt property for the user 
+//   // 2) If token has not expired, and there is a user, set the new password
+//   if (!user) { 
+//     return next(new AppError('Token is invalid or has expired', 400))
+//   }
+//   user.password = req.body.password;
+//   user.passwordConfirm = req.body.passwordConfirm;
+//   user.passwordResetToken = undefined;
+//   user.passwordResetExpires = undefined;
+//   await user.save();
+//   // 3) Update changedPasswordAt property for the user 
 
-  // 4) Log the user in, send JWT 
-  createSendToken(user, 200, res);
-});
+//   // 4) Log the user in, send JWT 
+//   createSendToken(user, 200, res);
+// });
 
-exports.updatePassword = catchAsync(async(req, res, next) => {
-  const user = await User.findById(req.user.id).select('+password');
+// exports.updatePassword = catchAsync(async(req, res, next) => {
+//   const user = await User.findById(req.user.id).select('+password');
 
-  if(!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
-    return next(new AppError('Your current password is wrong.', 401));
-  }
+//   if(!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
+//     return next(new AppError('Your current password is wrong.', 401));
+//   }
 
-  user.password = req.body.password;
-  user.passwordConfirm = req.body.passwordConfirm;
-  await user.save();
+//   user.password = req.body.password;
+//   user.passwordConfirm = req.body.passwordConfirm;
+//   await user.save();
 
-  createSendToken(user, 200, res);
-});
+//   createSendToken(user, 200, res);
+// });
--- utils/email.js
+++ utils/email.js
@@ -2,71 +2,7 @@ const nodemailer = require('nodemailer');
 const pug = require('pug');
 const htmlToText = require('html-to-text')
 
-module.exports = class Email {
-    constructor(user, url) {
-        this.to = user.email;
-        this.firstName = user.name.split('')[0];
-        this.url = url;
-        this.from = `Antonio <${process.env.EMAIL_FROM}>`;
-    }
-    newTransport() {
-        if(process.env.NODE_ENV === 'production') {
-            
-            return nodemailer.createTransport({
-                service: 'SendGrid',
-                auth: {
-                    user: process.env.SENDGRID_USERNAME,
-                    pass: process.env.SENDGRID_PASSOWORD
-                }
-            });
-        }
-        
-        return nodemailer.createTransport({
-            host: process.env.EMAIL_HOST,
-            port: process.env.EMAIL_PORT,
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_PASSWORD
-        }
-        });
-    }
-
-    async send(template, subject) {
-        const html = pug.renderFile(
-            `${__dirname}/../views/email/${template}.pug`, 
-            {
-                firstName: this.firstName,
-                url: this.url,
-                subject
-            }
-        
-        );
-
-        const mailOptions = {
-            from: this.from,
-            to: this.to,
-            subject,
-            html,
-            text: htmlToText.fromString(html)
-        };
-        
-        await this.newTransport().sendEmail(mailOptions)
-
-    }
-    async sendWelcome() {
-        await this.send('welcome','Welcome to our classes!');
-    }
-    async sendPasswordReset() {
-        await this.send(
-            'passWordReset',
-            'Your password reset token (valid for only 10 minutes)'
-            );
-    }
-};
-
-
 const sendEmail = async options => {
-    // 1) Create a transporter
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
@@ -74,17 +10,78 @@ const sendEmail = async options => {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
         }
-        // Activate in gmail "less secure app" option
     });
-    // 2) Define the email options 
     const mailOptions = {
-        from: 'Umreen Imam <hello@jonas.io>',
+        from: 'Jonas Schmedtmann <hello@jonas.io>',
         to: options.email,
         subject: options.subject,
-        text: options.message,
-    }
-    // 3) Send the email
+        text: options.message
+    };
+
     await transporter.sendMail(mailOptions)
 };
 
 module.exports = sendEmail;
+
+// module.exports = class Email {
+    //     constructor(user, url) {
+//         this.to = user.email;
+//         this.firstName = user.name.split('')[0];
+//         this.url = url;
+//         this.from = `Antonio <${process.env.EMAIL_FROM}>`;
+//     }
+//     newTransport() {
+//         if(process.env.NODE_ENV === 'production') {
+            
+//             return nodemailer.createTransport({
+//                 service: 'SendGrid',
+//                 auth: {
+//                     user: process.env.SENDGRID_USERNAME,
+//                     pass: process.env.SENDGRID_PASSOWORD
+//                 }
+//             });
+//         }
+        
+//         return nodemailer.createTransport({
+//             host: process.env.EMAIL_HOST,
+//             port: process.env.EMAIL_PORT,
+//             auth: {
+//                 user: process.env.EMAIL_USERNAME,
+//                 pass: process.env.EMAIL_PASSWORD
+//         }
+//         });
+//     }
+
+//     async send(template, subject) {
+//         const html = pug.renderFile(
+//             `${__dirname}/../views/email/${template}.pug`, 
+//             {
+//                 firstName: this.firstName,
+//                 url: this.url,
+//                 subject
+//             }
+        
+//         );
+
+//         const mailOptions = {
+//             from: this.from,
+//             to: this.to,
+//             subject,
+//             html,
+//             text: htmlToText.fromString(html)
+//         };
+        
+//         await this.newTransport().sendEmail(mailOptions)
+
+//     }
+//     async sendWelcome() {
+//         await this.send('welcome','Welcome to our classes!');
+//     }
+//     async sendPasswordReset() {
+//         await this.send(
+//             'passWordReset',
+//             'Your password reset token (valid for only 10 minutes)'
+//             );
+//     }
+// };
+
