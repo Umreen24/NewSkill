563aad5e37897045dda3c7fa141f03505c25c2ed
Taliaa <taliaa10@ymail.com>
2019-08-25 14:39:34 -0500

    admin btn styling

--- public/js/index.js
+++ public/js/index.js
@@ -18,15 +18,22 @@
 
 // updateSetting(form, 'data');
 
-let manageCourseMenu = document.querySelectorAll('.delete-svg');
-let manageCourseMenuList = document.querySelectorAll('.manage-course-menu');
+let manageCourseBtn = document.querySelectorAll('.delete-svg');
+let manageCourseMenu = document.querySelectorAll('.manage-course-menu');
 
-if (manageCourseMenu) {
-  manageCourseMenu.forEach(menu => {
-    // menu.addEventListener('click', showManageMenu);
+if (manageCourseBtn) {
+  manageCourseBtn.forEach(btn => {
+    // 1) Add event listener on each manage button
+    btn.addEventListener('click', function() {
+      // 2) Make the button show it's corresponding menu
+      showManageMenu(this);
+    });
   });
 }
 
-// function showManageMenu() {
-//   manageCourseMenuList.style.display = 'block';
-// }
+function showManageMenu(manageCourseBtn) {
+  let menuId = manageCourseBtn.dataset.menuid;
+  let menu = document.getElementById(menuId);
+  if (menu.style.display == 'none') menu.style.display = 'block';
+  else menu.style.display = 'none';
+}
