93aa696f2b0fcc287c7da4bf05b42297681f078a
Taliaa <taliaa10@ymail.com>
2019-08-25 03:19:17 -0500

    sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,38 +1,36 @@
 /* eslint-disable */
-  
-  const signUp = async (name, email, password, passwordConfirm) => {
-    try {
-      const res = await axios({
-        method: 'POST',
-        url: 'http://127.0.0.1:8000/api/v1/users/signup',
-        data: {
-            name,
-            email,
-            password,
-            passwordConfirm
-        }
-      });
-  
-      if (res.data.status) {
-        showAlert('success', 'Account created successfully!');
-        window.setTimeout(() => {
-        }, 1500);
+
+const signUp = async (name, email, password, passwordConfirm) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/signup',
+      data: {
+        name,
+        email,
+        password,
+        passwordConfirm
       }
-    } catch (err) {
-      showAlert('error', err.response.data.message);
-    }
-  };
-  
-  const signUpForm = document.querySelector('.form--signUp')
-  
-  if (signUpForm) {
-    signUpForm.addEventListener('submit', e => {
-      e.preventDefault();
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-      const passwordConfirm = document.getElementById('passwordConfirm').value;
-      signUp(name, email, password, passwordConfirm);
     });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Account created successfully!');
+      window.setTimeout(() => {}, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err);
   }
-  
+};
+
+const signUpForm = document.querySelector('.form--signUp');
+
+if (signUpForm) {
+  signUpForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signUp(name, email, password, passwordConfirm);
+  });
+}
